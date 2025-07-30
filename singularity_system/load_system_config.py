8cecb99539df0aae0456e5ac4c2967c1dba818e3
dbanco <dpqb10@gmail.com>
2025-07-30 12:01:43 -0700

    update

--- singularity_system/load_system_config.py
+++ singularity_system/load_system_config.py
@@ -14,20 +14,23 @@ with open(yaml_file, "r") as f:
 
 sys_cfg = config["system"]
 detect_cfg = config["detector"]
-base_dir = sys_cfg["base_dir"]
+mtt_dir = sys_cfg["mtt_dir"]
+sing_dir = sys_cfg["singularity_dir"]
 
-print(f'export SYS_DIR="{base_dir}"')
+
+print(f'export MTT_DIR="{mtt_dir}"')
+print(f'export SING_DIR="{os.path.join(mtt_dir, sing_dir)}"')
 print(f'export REDIS_HOST="{sys_cfg["redis_host"]}"')
 print(f'export REDIS_PORT={sys_cfg["redis_port"]}')
 print(f'export POSTGRES_HOST="{sys_cfg["postgres_host"]}"')
 print(f'export USER="{sys_cfg["user"]}"')
 
-print(f'export SIF_DIR="{os.path.join(base_dir, sys_cfg["sif_dir"])}"')
-print(f'export APP_DIR="{os.path.join(base_dir, sys_cfg["app_dir"])}"')
+print(f'export SIF_DIR="{os.path.join(sing_dir, sys_cfg["sif_dir"])}"')
+print(f'export APP_DIR="{os.path.join(sing_dir, sys_cfg["app_dir"])}"')
 print(f'export DB_NAME="{sys_cfg["db_name"]}"')
-print(f'export POSTGRES_DIR={os.path.join(base_dir, sys_cfg["postgres_dir"])}')
-print(f'export REGION_DIR={os.path.join(base_dir, sys_cfg["region_dir"])}')
-print(f'export TRACKER_STATE_DIR={os.path.join(base_dir, sys_cfg["tracker_state_dir"])}')
+print(f'export POSTGRES_DIR={os.path.join(sing_dir, sys_cfg["postgres_dir"])}')
+print(f'export REGION_DIR={os.path.join(sing_dir, sys_cfg["region_dir"])}')
+print(f'export TRACKER_STATE_DIR={os.path.join(sing_dir, sys_cfg["tracker_state_dir"])}')
 print(f'export NUM_TRACKERS={sys_cfg["num_trackers"]}')
 
 print(f'export DETECT_YAML_DIR={detect_cfg["yaml_dir"]}')
